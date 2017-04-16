5243d6b264d6768a45e4e1d65746828b182cd406
Andrew J Wagner <andrew@drewagllc.com>
2017-04-15 22:22:03 -0600

    [UP] Update to SwiftServe 6.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftServeKitura",
     dependencies: [
-        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 5),
+        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 6),
 
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 14),

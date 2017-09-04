ffa543916914a87bb7496b9a55c4c7321a558fc6
Andrew Wagner <andrew@onbeat.fit>
2017-09-03 21:37:47 -0600

    [UP] Updated to Kitura 1.7

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 7),
 
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/Zewo/File.git", majorVersion: 0, minor: 14),
     ]
 )

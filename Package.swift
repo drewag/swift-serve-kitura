72c60a9fca73c12790933bf3cbbb1bd1f3ac03e0
Andrew J Wagner <drewag@gmail.com>
2017-12-11 11:45:02 -0700

    [BF] Properly support SwiftServe 10.0.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftServeKitura",
     dependencies: [
-        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 9),
+        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 10),
 
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 0),
     ]

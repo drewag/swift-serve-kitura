3c07caf7d9e04993ed2e6f521d3ab5e8f7ef518e
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-05-13 21:53:26 -0600

    [UP] Update to Swift Serve 13

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftServeKitura",
     dependencies: [
-        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 12),
+        .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 13),
 
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 2),
     ]

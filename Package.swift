e4b977fb79125faf0e3c1f4cff7fdef76c80f820
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-11-04 12:51:29 -0700

    [UP] Update to Kitura 2.5.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/drewag/swift-serve.git", majorVersion: 13),
 
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 2),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 5),
     ]
 )

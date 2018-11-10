2d7860397aa8d48d8a0e1d169fdb609807dea881
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-11-09 20:58:30 -0700

    [UP] Update to SwiftServe 14

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SwiftServeKitura", targets: ["SwiftServeKitura"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/drewag/swift-serve.git", from: "13.0.0"),
+        .package(url: "https://github.com/drewag/swift-serve.git", from: "14.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.5.0"),
     ],
     targets: [

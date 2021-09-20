27382d0a25d8e6a7b9b06433ee869dda8e482165
Andrew Wagner <845683+drewag@users.noreply.github.com>
2021-09-19 17:41:46 -0700

    [UP] Update to Swift 5.4

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 let package = Package(
     name: "SwiftServeKitura",
     platforms: [
-        .macOS(.v10_11),
+        .macOS(.v10_12),
     ],
     products: [
         .library(name: "SwiftServeKitura", targets: ["SwiftServeKitura"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/drewag/swift-serve.git", from: "18.0.0"),
+        .package(url: "https://github.com/drewag/swift-serve.git", from: "19.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.5.0"),
     ],
     targets: [
